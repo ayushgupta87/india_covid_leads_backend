1e2f4713f7a95d597bf813e368192f5965fd684d
ayushgupta <ayushgupta304@gmail.com>
2021-05-02 20:25:50 +0530

    Edits for digital ocean

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_jwt_extended import JWTManager
 from werkzeug.serving import WSGIRequestHandler
 
@@ -18,8 +18,14 @@ api = Api(app)
 jwt = JWTManager(app)
 
 
-if __name__ == '__main__':
+class HomePage(Resource):
+    def get(self):
+        return 'Hello world'
 
+
+api.add_resource(HomePage, '/')
+
+if __name__ == '__main__':
     # Flutter error of Connection closed while receiving data > windows OS
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
     app.run(debug=True, port=5000)
